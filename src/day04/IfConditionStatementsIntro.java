233d2495e49139cb4e74fb3c7a96e6f62a0abdbf
kenanhs24 <170270713+kenanhs24@users.noreply.github.com>
2024-08-13 20:35:54 -0400

    If_condition_and_statement

---
+++ src/day04/IfConditionStatementsIntro.java
@@ -0,0 +1,12 @@
+package day04;
+
+public class IfConditionStatementsIntro {
+    public static void main(String[] args) {
+
+        int person_age=25;
+        if(person_age>=18){
+            System.out.println("You are older than 18");
+        }
+    }
+}
+
