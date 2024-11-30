89a9b3eb595189f97fd581dda879ec420c9e6c47
kenanhs24 <170270713+kenanhs24@users.noreply.github.com>
2024-11-30 00:24:57 -0500

    Hello world

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,8 @@
-public class HelloWorld {
-    public static void main(String[] args) {
+public class HelloWorld
+{
+    // New class main
+    public static void main(String[] args)
+    {
         // Print Hello, World! to the console
         System.out.println("Hello, World!");
     }
